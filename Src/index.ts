33523c7494cba6d949681463c4feed338be89848
HassanSalah01 <hassan.salah.eldine1@gmail.com>
2022-09-03 21:07:22 +0200

    Updated_index.ts

--- Src/index.ts
+++ Src/index.ts
@@ -1,7 +1,16 @@
-export const foo = (x:number):number=>{
-    return x+x;
-}
+import  express ,{Request,Response,Express} from "express";
+const app:Express = express();
+const port:number = 3000;
 
-export const naming = (x:string):string =>{
-    return x;
-}
+app.get('/:id/:name',(req:Request,res:Response):void=>{
+    console.log(req.params);
+    res.status(200);
+    res.json({name:"ahmed",age:"15"})
+})
+
+
+
+
+app.listen(port,()=>{
+    console.log(`server is running on port ${3000}`)
+})
