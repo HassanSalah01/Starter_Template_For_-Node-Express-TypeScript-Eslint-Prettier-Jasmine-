38c7dba0cb8840a1f4278923f4d931dae787689b
HassanSalah01 <hassan.salah.eldine1@gmail.com>
2022-10-15 20:29:38 +0200

    Added_DOTENV_Updated

--- Src/index.ts
+++ Src/index.ts
@@ -1,5 +1,8 @@
 import express, { Request, Response, Application } from "express";
+import dotenv from "dotenv";
 const app: Application = express();
+dotenv.config();
+
 const PORT = 3000;
 
 app.get("/", (req: Request, res: Response) => {
